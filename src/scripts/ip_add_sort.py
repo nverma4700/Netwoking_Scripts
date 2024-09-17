24061293c59c2a1f43279d3c0c23d48202386add
Nishant Verma <57328381+nverma4700@users.noreply.github.com>
2024-09-17 15:39:08 +0530

    Adding sorted IP script

---
+++ src/scripts/ip_add_sort.py
@@ -0,0 +1,31 @@
+"""
+Task:Given a list of IP addresses, sort them by their numerical value.
+Sample Input: 
+['192.168.1.3', '10.0.0.10', '172.16.0.0', '192.168.0.100']
+Sample Output:
+['10.0.0.1', '172.16.0.1', '192.168.0.100', '192.168.1.1']
+"""
+
+import ipaddress
+
+def sort_ip_address(ip_list):
+    '''
+    Will take a ip_list and sort it out based on the integer value of the ip address
+    '''
+    sorted_ip_addresses = sorted(ip_list, key=lambda ip: int(ipaddress.IPv4Address(ip)))
+    return sorted_ip_addresses
+
+
+def sort_ip_last_octect(ip_list):
+    '''
+    Will take a ip_list and sort it out based on last octet value of the ip address
+    '''
+    sorted_ip_last_octet = sorted(ip_list, key=lambda ip: int(ip.split('.')[3]))
+    return sorted_ip_last_octet
+
+if __name__ == '__main__':
+    ip_addresses = ['192.168.1.3', '10.0.0.10', '172.16.0.0', '192.168.0.100']
+    sorted_ips = sort_ip_address(ip_addresses)
+    last_octet_sorted_ips = sort_ip_last_octect(ip_addresses)
+    print(sorted_ips)
+    print(last_octet_sorted_ips)
